951a333dde6f8b499315f24c80ad1c4888b68d47
CJCali2005 <235065072+CJCali2005@users.noreply.github.com>
2026-08-28 11:42:37 +0200

    Features(Drone):Drone fonctionnel sans fonction

---
+++ Personal-work/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////// Déclaration de variables ////////////////////////////////////////////
+
+            string droneVivant = "x-O-x";
+            string droneMort = "_____";
+            int nvBatterieDebut = 0;
+            int nvBatterie = 100;
+            int Pos_x = 0;
+
+            const int PERTE_DE_VIE = 2;
+            const int POS_Y = 10;
+
+
+            ////////////////////////////////////////// Début du programme ////////////////////////////////////////////////////
+
+
+            while (nvBatterie > 0)
+            {
+
+
+                Console.Clear();
+
+                Pos_x++;
+                Console.SetCursorPosition(Pos_x, POS_Y);
+                Console.WriteLine(droneVivant);
+
+                nvBatterie = nvBatterie - 2;
+                Thread.Sleep(100);
+
+            }
+
+            Console.Clear();
+            Console.SetCursorPosition(Pos_x, POS_Y);
+            Console.WriteLine(droneMort);
+
+
+
+        }
+    }
+}
