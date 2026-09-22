a3719bc3cb383a1db2ec12b8f794342b27ca3813
CJCali2005 <235065072+CJCali2005@users.noreply.github.com>
2026-09-22 17:10:46 +0200

    Modification(RandomValueHelper):Création de la classe de génération de valeur aléatoires
    [20][DONE]
    
    Je Re-Commit car je n'avais pas bien saisi la consigne voila la version modfiée

---
+++ Personal-work/Drones/Drones/Helpers/RandomValueHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Helpers
+{
+    internal static class RandomHelper
+    {
+
+        private static readonly Random random = new Random();
+
+        public static int GetRandom(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+    }
+    
+}
+
--- Personal-work/Drones/Drones/Model/Drone.cs
+++ Personal-work/Drones/Drones/Model/Drone.cs
@@ -38,12 +38,6 @@ namespace Drones
             charge = alea.Next(1000); // La charge initiale de la batterie est choisie aléatoirement
         }
 
-        // géneration de valeur aléatoire
-        public static int RandomValue()
-        {
-            int random = random.Next();
-            return random;
-        }
 
         #region ================ Modelisation du drone et de son comportement ================
 
